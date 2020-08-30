892ce9de0647dfecdecc092c6a41e71ec8ea8bf4
何志刚 <zhiganghe1201@outlook.com>
2020-08-31 00:07:18 +0800

    feat: update prompt

--- config/prompt.config.js
+++ config/prompt.config.js
@@ -1,12 +1,33 @@
 const { prompt } = require('inquirer');
 const fs = require('fs-extra');
+const { exec } = require('shelljs');
 
 const question = [
+	{
+		type: 'confirm',
+		name: 'install',
+		message: 'npm install?',
+		default: false
+	},
 	{
 		type: 'input',
 		name: 'port',
 		message: 'port',
-		default: '8082'
+		default: '8082',
+		choices(answers) {
+			if (answers.install) {
+				let done = this.async();
+				exec('yarn', { silent: true }, (code, stdout, stderr) => {
+					console.log('Exit code:', code);
+					console.log('Program output:', stdout);
+					console.log('Program stderr:', stderr);
+					if (code === 0) {
+						done(null, true);
+					}
+
+				});
+			}
+		}
 	}
 ];
 
