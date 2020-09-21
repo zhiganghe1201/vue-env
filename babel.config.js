6d4d7a9aeaf44029352dba6ca0c5854661fc37ac
何志刚 <zhiganghe1201@outlook.com>
2020-09-21 22:46:19 +0800

    docs: 增加注释

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	],
 	'plugins': [
 		['@babel/plugin-transform-runtime', {
-			corejs: 3
+			corejs: 3 // 这里corejs 和 presets里面设置的corejs 不同, 这里的corejs 指定了一个叫runtime-corejs的库版本, 使用时也需npm安装对应的包 npm i @babel/runtime-corejs3
 		}],
 		'@babel/plugin-syntax-dynamic-import',
 		'transform-vue-jsx',
